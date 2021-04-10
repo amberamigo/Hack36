c216309dbc813b89f303bf005ce7eed4857f9a66
Srivats <agarwalsrivatsh26@gmail.com>
2021-04-10 16:21:28 +0530

    Added Services

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const _ = require("lodash");
 mongoose.connect("mongodb://localhost:27017/hack36DB", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 
-Sndkslsksl
+// Sndkslsksl
 
 const itemSchema = {
   name : String,
